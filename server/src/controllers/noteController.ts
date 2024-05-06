7287b6bc09597fe8105848ff65f8f3d2f1580401
muhammad-jiku <muhammadjiku364@gmail.com>
2024-05-07 00:14:34 +0600

    update note func added

--- server/src/controllers/noteController.ts
+++ server/src/controllers/noteController.ts
@@ -4,6 +4,92 @@ import mongoose from 'mongoose';
 import createHttpError from 'http-errors';
 import NoteModel from '../models/Note';
 
+interface CreateNoteBody {
+  title?: string;
+  text?: string;
+}
+
+export const createNote: RequestHandler<
+  unknown,
+  unknown,
+  CreateNoteBody,
+  unknown
+> = async (req, res, next) => {
+  const title = req.body.title;
+  const text = req.body.text;
+  const authenticatedUserId = req.session.userId;
+
+  try {
+    isAssertDefined(authenticatedUserId);
+
+    if (!title) {
+      throw createHttpError(400, 'Note must have a title');
+    }
+
+    const newNote = await NoteModel.create({
+      userId: authenticatedUserId,
+      title: title,
+      text: text,
+    });
+
+    res.status(201).json(newNote);
+  } catch (error) {
+    next(error);
+  }
+};
+
+interface UpdateNoteParams {
+  noteId: string;
+}
+
+interface UpdateNoteBody {
+  title?: string;
+  text?: string;
+}
+
+export const updateNote: RequestHandler<
+  UpdateNoteParams,
+  unknown,
+  UpdateNoteBody,
+  unknown
+> = async (req, res, next) => {
+  const noteId = req.params.noteId;
+  const newTitle = req.body.title;
+  const newText = req.body.text;
+  const authenticatedUserId = req.session.userId;
+
+  try {
+    isAssertDefined(authenticatedUserId);
+
+    if (!mongoose.isValidObjectId(noteId)) {
+      throw createHttpError(400, 'Invalid note id');
+    }
+
+    if (!newTitle) {
+      throw createHttpError(400, 'Note must have a title');
+    }
+
+    const note = await NoteModel.findById(noteId).exec();
+
+    if (!note) {
+      throw createHttpError(404, 'Note not found');
+    }
+
+    if (!note.userId.equals(authenticatedUserId)) {
+      throw createHttpError(401, 'You cannot access this note');
+    }
+
+    note.title = newTitle;
+    note.text = newText;
+
+    const updatedNote = await note.save();
+
+    res.status(200).json(updatedNote);
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const getNotes: RequestHandler = async (req, res, next) => {
   const authenticatedUserId = req.session.userId;
 
@@ -39,40 +125,6 @@ export const getNote: RequestHandler = async (req, res, next) => {
     }
 
     res.status(200).json(note);
-  } catch (error) {
-    next(error);
-  }
-};
-
-interface CreateNoteBody {
-  title?: string;
-  text?: string;
-}
-
-export const createNote: RequestHandler<
-  unknown,
-  unknown,
-  CreateNoteBody,
-  unknown
-> = async (req, res, next) => {
-  const title = req.body.title;
-  const text = req.body.text;
-  const authenticatedUserId = req.session.userId;
-
-  try {
-    isAssertDefined(authenticatedUserId);
-
-    if (!title) {
-      throw createHttpError(400, 'Note must have a title');
-    }
-
-    const newNote = await NoteModel.create({
-      userId: authenticatedUserId,
-      title: title,
-      text: text,
-    });
-
-    res.status(201).json(newNote);
   } catch (error) {
     next(error);
   }
